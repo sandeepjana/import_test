f0640f7eb4c219f087455a047a68e296806babf5
sandeepjana <4102043+sandeepjana@users.noreply.github.com>
2023-02-19 18:01:28 +0530

    fix the entry point

--- pkg/__main__.py
+++ pkg/__main__.py
@@ -5,16 +5,21 @@ def warn(e, msg):
     print(f':cross_mark: [yellow]{e}: [bold]{msg}[/][/]')
 
 
-try:
-    msg = "from pkg.bg.bg_src import fun"
-    from pkg.bg.bg_src import fun
-    fun(msg)
-except ImportError as e:
-    warn(e, msg)
+def main():
+    try:
+        msg = "from pkg.bg.bg_src import fun"
+        from pkg.bg.bg_src import fun
+        fun(msg)
+    except ImportError as e:
+        warn(e, msg)
 
-try:
-    msg = "from .bg.bg_src import fun"
-    from .bg.bg_src import fun
-    fun(msg)
-except ImportError as e:
-    warn(e, msg)
+    try:
+        msg = "from .bg.bg_src import fun"
+        from .bg.bg_src import fun
+        fun(msg)
+    except ImportError as e:
+        warn(e, msg)
+
+
+if __name__ == '__main__':
+    main()
